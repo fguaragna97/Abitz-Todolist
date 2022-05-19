da4d4cbf9f078f537095859ce8dae3e31a01b7b5
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-19 00:54:47 -0700

    Create the login and register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <h2 className="my-4">Login</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Log in
+        </Button>
+      </Form>
+    </>
+  );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <>
+      <h2 className="my-4">Register</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </>
+  );
 }
