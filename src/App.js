7c8f067dcc72593fffbf34f05445578fbccc36ba
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-18 22:10:51 -0700

    install react-router and create pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+
 import NavbarMenu from "./components/NavbarMenu";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -7,7 +12,13 @@ function App() {
       <NavbarMenu></NavbarMenu>
       <Container>
         <Row>
-          <Col></Col>
+          <Col>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Col>
         </Row>
       </Container>
     </>
--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function NavbarMenu() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Abitz</Navbar.Brand>
+        <Link to="/" className="navbar-brand">
+          Abitz
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link href="#home">Log in</Nav.Link>
-            <Nav.Link href="#link">Register</Nav.Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+            <Link to="/register" className="nav-link">
+              Register
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
