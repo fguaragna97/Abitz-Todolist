0ef5a703013fab3416f2d1ffdcdb41e7e427b96a
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-05-27 00:25:12 -0700

    delete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../App";
 
 function Task({ taskName, completed, id }) {
+  const { user, tasks, setTask } = useContext(UserContext);
   let navigate = useNavigate();
+
   const onEdit = (event) => {
     event.preventDefault();
     navigate(`/task/edit/${id}`);
+  };
+
+  const onDelete = async (event) => {
+    event.preventDefault();
+    try {
+      await fetch(`http://localhost:3001/home/tasks/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify(),
+      });
+      const fetchdata = async () => {
+        try {
+          const response = await fetch("http://localhost:3001/home/alltask", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: localStorage.getItem("token"),
+            },
+          });
+          const apidata = await response.json();
+          setTask(apidata);
+        } catch (error) {}
+      };
+      fetchdata();
+    } catch (error) {}
   };
 
   return (
@@ -20,6 +51,11 @@ function Task({ taskName, completed, id }) {
         </Col>
         <Col>
           <Button onClick={onEdit}>Edit</Button>
+        </Col>
+        <Col>
+          <Button onClick={onDelete} variant="danger">
+            Delete
+          </Button>
         </Col>
       </Row>
       <Row className="divider"></Row>
--- src/pages/EditTask.js
+++ src/pages/EditTask.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function EditTask() {
+  let navigate = useNavigate();
   const params = useParams();
   const [edit, setEdit] = useState({});
+
   useEffect(() => {
     const fetchdata = async () => {
       try {
@@ -25,34 +28,41 @@ export default function EditTask() {
     fetchdata();
   }, []);
 
-  const handleEdit = () => {
+  console.log(params.id);
+
+  const handleEdit = async (event) => {
+    event.preventDefault();
+    const newdata = {
+      taskName: edit.taskName,
+    };
+
     try {
-      const response = await fetch(`http://localhost:3001/home/tasks/${params.id}`, {
+      await fetch(`http://localhost:3001/home/tasks/${params.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(newdata),
       });
-      const apidata = await response.json();
-      if (apidata.error) {
-        console.log("User doesnt exist");
-      } else {
-        localStorage.setItem("token", apidata.token);
-        setUser(apidata.user);
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+      navigate("/");
+    } catch (error) {}
   };
+
+  const handleChange = (event) => {
+    setEdit({
+      ...edit,
+      taskName: event.target.value,
+    });
+  };
+
   return (
     <>
       <h2 className="my-4">New Task</h2>
       <Form onSubmit={handleEdit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control name="taskName" value={edit.taskName} />
+          <Form.Control value={edit.taskName} onChange={handleChange} />
         </Form.Group>
         <Button variant="primary" type="submit">
           Save Changes
